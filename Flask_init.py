c5997fe0cd84f03150a25bf187ca726be5c6880e
bemma <mbem@student.agh.edu.pl>
2025-04-11 18:42:49 +0200

    added plotting plottting functions with one column argument in pdf

--- Flask_init.py
+++ Flask_init.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, request, jsonify, send_file
 import os
 import importlib.util
@@ -8,12 +7,14 @@ import Functions.analysys_functions
 import Functions.plotting_functions
 import pandas as pd
 from fpdf import FPDF
-
+import matplotlib.pyplot as plt
+from sympy.physics.units import coulomb
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -30,7 +31,8 @@ def index():
             df = load_csv_file(uploaded_file)
             columns = df.columns.tolist()
 
-    return render_template('index.html', functions=functions , columns=columns, uploaded_file=uploaded_file)
+    return render_template('index.html', functions=functions, columns=columns, uploaded_file=uploaded_file)
+
 
 @app.route("/execute", methods=["POST"])
 def execute():
@@ -49,26 +51,34 @@ def execute():
     if df.empty:
         print("No data found")
 
-    results = {}
-    for func_name, args in args_map.items():
-        if func_name in functions:
-            func = functions[func_name]
-            try:
-                results[func_name] = [func(df, *args_map[func_name]), *args_map[func_name]]
-            except Exception as e:
-                results[func_name] = f"Error executing {func_name}: {str(e)}"
-
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial", size=12)
     pdf.cell(200, 10, txt="Results of data analysis", ln=True, align="C")
-    for func_name, result in results.items():
-        pdf.cell(200, 10, txt=f"{func_name} of column: {result[1]}: {result[0]}", ln=True, align="L")
+
+    for func_name, args in args_map.items():
+        if func_name in functions:
+            func = functions[func_name]
+            try:
+                columns = args_map[func_name]
+                result = func(df, *columns)
+                if isinstance(result, plt.Figure):
+                    fig_path = os.path.join(UPLOAD_FOLDER, f"{func_name}.png")
+                    result.savefig(fig_path)
+                    plt.close(result)
+                    pdf.cell(200, 10, txt=f"{func_name} of column: {', '.join(map(str, columns))}:", ln=True, align="L")
+                    pdf.image(fig_path, x=10, y=None, w=180)
+                else:
+                    pdf.cell(200, 10, txt=f"{func_name} of column: {', '.join(map(str, columns))}: {result}", ln=True, align="L")
+            except Exception as e:
+                pdf.cell(200, 10, txt=f"Error generating result for {func_name}: {str(e)}", ln=True, align="L")
+
 
     pdf_path = os.path.join(UPLOAD_FOLDER, "results.pdf")
     pdf.output(pdf_path)
 
     return send_file(pdf_path, as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
