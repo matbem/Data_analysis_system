6cf697133a999bab47802bd20d2da5eb5c00dc99
bemma <mbem@student.agh.edu.pl>
2025-04-15 16:26:40 +0200

    final changes

--- Functions/analysys_functions.py
+++ Functions/analysys_functions.py
@@ -20,12 +20,6 @@ def compute_median(df, column_name):
                 else (sorted_col[len(sorted_col)//2] + sorted_col[len(sorted_col)//2 - 1]) / 2)
             (sorted(df[column_name])))
 
-def compute_percentiles(df, column_name, percentile):
-    sorted_col = sorted(df[column_name])
-    index = int(percentile / 100 * (len(sorted_col) - 1))
-    return sorted_col[index]  # ta funkcje troche zmienilem bo mi wywalalo blad przez nia
-
-
 def compute_squared_difference(df,column_name):
     return list(map(lambda x: (x - compute_mean(df, column_name))**2, df[column_name]))
 
@@ -42,9 +36,7 @@ def compute_covariance(df, column_name1, column_name2):
         ) / (len(df[column_name1]) - 1)
 
 def factorial_column(df, column_name):
-    if df[column_name].empty:
-        return df[column_name]
-    return df[column_name].apply(lambda x: math.factorial(int(x)) if x >= 0 else None) # i tutaj zmienilem ta koncowke po .apply
+    return df[column_name].apply(lambda x: None if x<0 else reduce(lambda acc, n: acc*n, range(1, int(x)+1),1))
 
 def recursive_sum(df, column_name):
     return lambda: 0 if df.empty else df[column_name].iloc[0] + recursive_sum(df.iloc[1:], column_name)()
@@ -59,7 +51,6 @@ def get_all_functions():
         "Mean": compute_mean,
         "Standard deviation": compute_std,
         "Median": compute_median,
-        "Percentiles": compute_percentiles,
         "Squared difference": compute_squared_difference,
         "Variance": compute_variance,
         "Covariance": compute_covariance,
